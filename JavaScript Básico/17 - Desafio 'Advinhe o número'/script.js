d2a8ee23ff33428194f0dfe9ef4774b64e02160d
Leocaliban <leocaliban@gmail.com>
2018-06-12 12:44:26 -0300

    JavaScript - Desafio 'Advinhe o número'

---
+++ JavaScript Básico/17 - Desafio 'Advinhe o número'/script.js
@@ -0,0 +1,20 @@
+function verificar(){
+    var n1 = document.getElementById('n1').innerHTML;
+    var n2 = document.getElementById('n2').value;
+
+    if(n1 == n2){
+        alert("Você acertou o número!");
+    }
+    else{
+        alert("Você errou o número!")
+    }
+
+    resetar();
+}
+
+
+function resetar(){
+    document.getElementById('n2').value = "";
+    var random = Math.floor(Math.random() * 10);
+    document.getElementById('n1').innerHTML = random;
+}
