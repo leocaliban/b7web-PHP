5d9b52ee941ea9c461e8023f7e947358ac848270
Leocaliban <leocaliban@gmail.com>
2018-06-12 09:29:24 -0300

    JavaScript - Modificando Listas

---
+++ JavaScript Básico/10 - Modificando Listas/script.js
@@ -0,0 +1,8 @@
+function adicionarItem(){
+    var item = document.getElementById('item').value;
+    var lista = document.getElementById('lista').innerHTML;
+
+    lista = lista + '<li>'+item+'</li>';
+
+    document.getElementById('lista').innerHTML = lista;
+}
