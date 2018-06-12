3fa7552f371c48312b62b8a41fa8ff7841790712
Leocaliban <leocaliban@gmail.com>
2018-06-12 12:21:27 -0300

    JavaScript - Switch Case

---
+++ JavaScript Básico/16 - Switch/script.js
@@ -0,0 +1,15 @@
+var x = 0;
+
+switch(x){
+    case 0:
+        alert("x é zero");
+        break;
+    case 1:
+        alert("x é um");
+        break;
+    case 2:
+        alert("x é dois");
+        break;
+    default:
+        alert("Nenhum case foi satisfeito.");
+}
