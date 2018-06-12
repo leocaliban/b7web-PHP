2520374e3d5a895cdb1014e0b69943f6b38179c7
Leocaliban <leocaliban@gmail.com>
2018-06-12 11:40:35 -0300

    JavaScript - Verificadores

---
+++ JavaScript Básico/14 - Verificadores/script.js
@@ -0,0 +1,10 @@
+function verificador(){
+    var x = prompt("Digite um número:");
+
+    if(x > 10) {
+        alert("O número " + x + " é maior que 10!");
+    }
+    else{
+        alert("O número " + x + " não é maior que 10!");
+    }
+}
