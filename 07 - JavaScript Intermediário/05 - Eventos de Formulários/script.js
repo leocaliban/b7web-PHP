4b4d9b1bc696fa0155f0958a7f473eb216c05cdb
Leocaliban <leocaliban@gmail.com>
2018-06-14 15:31:54 -0300

    JavaScript - Eventos de Formulário

---
+++ 07 - JavaScript Intermediário/05 - Eventos de Formulários/script.js
@@ -0,0 +1,11 @@
+function mudouOpcao(objeto){
+    console.log('Seleção do usuário: ' + objeto.value);
+}
+
+function campoEmFoco(){
+    console.log('Campo focado');
+}
+
+function saiuDeFoco(){
+    console.log('Campo perdeu o foco.');
+}
