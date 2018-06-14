d2109a7c5e1916e7844f597f6d3bfdaa8e303e8b
Leocaliban <leocaliban@gmail.com>
2018-06-14 15:03:47 -0300

    JavaScript - Aviso ao deixar site[D]

--- 07 - JavaScript Intermediário/04 - Eventos de Página/script.js
+++ 07 - JavaScript Intermediário/04 - Eventos de Página/script.js
@@ -1,3 +1,11 @@
 function carregou(){
     alert('Página carregada');
 }
+
+/** jQuerry para alertar quando o usuário for abandonar a página
+ * $(document).bind("mouseleave", function(e) {
+    if (e.pageY - $(window).scrollTop() <= 1) {    
+        alert('Tem certeza que quer fechar a aba? :(');
+    }
+});
+ */
